b6839bb58d389a72e14343bbe5a070f17f476a79
Fletcher Nichol <fnichol@nichol.ca>
2013-01-23 00:46:41 -0700

    Implement 'flacky to_mp3'.

--- lib/flacky/cli.rb
+++ lib/flacky/cli.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 
+require 'benchmark'
 require 'json'
 require 'thor'
 
@@ -38,6 +39,90 @@ module Flacky
         print files.join("\x0").concat("\x0")
       else
         puts files.join("\n") unless files.empty?
+      end
+    end
+
+    desc "to_mp3 [file ...]|[**/*.flac ...]", "Convert Flac files to MP3 files"
+    method_option :destination, :aliases => "-d",
+      :desc => "Sets optional destination directory"
+    method_option :'lame-opts', :aliases => "-l",
+      :default => "--vbr-new -V 0 -b 320",
+      :desc => "Set the lame encoding arguments"
+    def to_mp3(*args)
+      %w{flac metaflac lame}.each do |cmd|
+        abort "Command #{cmd} must be on your PATH" unless %x{which #{cmd}}
+      end
+
+      args.each do |glob|
+        FlacToMp3.new(
+          :glob       => glob,
+          :lame_opts  => options[:'lame-opts'],
+          :dest_root  => options[:destination]
+        ).convert!
+      end
+    end
+
+    private
+
+    class FlacToMp3
+
+      def initialize(opts = {})
+        @glob = opts[:glob]
+        @lame_opts = opts[:lame_opts]
+        @dest_root = opts[:dest_root]
+      end
+
+      def convert!
+        Dir.glob(glob).each do |file|
+          next unless file =~ /\.flac$/
+          convert_file(file)
+        end
+      end
+
+      private
+
+      attr_reader :glob, :lame_opts, :dest_root
+
+      def convert_file(file)
+        dst = file.sub(/\.flac$/, ".mp3")
+        if dest_root
+          dst = File.join(dest_root, dst)
+          FileUtils.mkdir_p File.dirname(dst)
+        end
+
+        cmd = %{flac -dcs "#{file}" | lame #{lame_opts}}
+        tags.each { |lt, ft| cmd << %{ --#{lt} "#{tag(file, ft)}"} }
+        cmd << %{ - "#{dst}"}
+
+        banner "Processing #{file}..."
+        elapsed = Benchmark.measure do
+          %x{#{cmd}}
+        end
+        banner "Wrote out #{dst} #{duration(elapsed.real)}"
+      end
+
+      def tags
+        { :tt => :title, :tl => :album, :ta => :artist, :tn => :tracknumber,
+          :tg => :genre, :tc => :comment, :ty => :date }.freeze
+      end
+
+      def tag(file, tag)
+        r = %x{metaflac --show-tag=#{tag.to_s.upcase} "#{file}"}
+        unless r.nil? || r.empty?
+          r.split("=").last.chomp
+        else
+          ""
+        end
+      end
+
+      def duration(total)
+        minutes = (total / 60).to_i
+        seconds = (total - (minutes * 60))
+        "(%dm%.2fs)" % [minutes, seconds]
+      end
+
+      def banner(msg)
+        puts "-----> #{msg}"
       end
     end
   end
